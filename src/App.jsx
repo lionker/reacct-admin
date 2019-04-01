189c31f437d4a4d7cf81d0d6f270e914b900f4de
Huang56 <635837244@qq.com>
2019-04-01 20:54:36 +0800

    'first-commit'

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@
  * 应用主组件
  */
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 
-
+import Login from './pages/login/index'
+import Admin from './pages/admin/index'
 
 // 测试高阶组件
 // import A from './test.1/login';
@@ -15,9 +17,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        {/* {<A />} */}
-        {/* {<B />} */}
-        
+        <Switch>
+          <Route path="/login" component={Login} />
+          {/* 为了开发login组件设计的 */}
+          {/* <Redirect to="/login" /> */}
+          <Route path="/" component={Admin} />
+        </Switch>
       </div>
     )
   }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,31 +4,94 @@
 
 import React, { Component } from 'react'
 import {
-    Form, Icon, input, Buton
-} from 'antd'
+    Form, Icon, Input, Button
+} from 'antd'    //按需引入
 
-import log from './logo.png'
+import logo from './logo.png'
 import './index.less'
 
-const Item = Form.item;   //?
+const Item = Form.Item;   //替换成Item
 
-export default class Login extends Component {
+
+@Form.create()
+class Login extends Component {
 
     login = (e) => {
         e.preventDefault();
     }
+    /** 
+     * 自定义表单的校验规则
+    */
+    validator = (rule, value, callback) => {
+        // console.log(rule, value);
+        const length = value && value.length;
+        const pwdReg = /^[a-zA-Z0-9_]+$/;
+        if (!value) {
+            // callback如果不传参代表校验成功，如果传参代表校验失败，并且会提示错误
+            callback('必须输入密码');
+        } else if (length < 4) {
+            callback('密码必须大于4位');
+        } else if (length > 12) {
+            callback('密码必须小于12位');
+        } else if (!pwdReg.test(value)) {
+            callback('密码必须是英文、数组或下划线组成');
+        } else {
+            // 必须调用callback
+            callback();
+        }
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;  //引入验证函数
         return (
             <div className="login">
                 <header className="login-header">
-                    <img src="logo" alt="log"/>
+                    <img src={logo} alt="log0" />
                     <h1>react项目: 后台管理系统</h1>
                 </header>
                 <section className="login-content">
                     <h3>用户登录</h3>
-                    Form[onSubmit=this.login className=login-form]
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            {
+                                // getFieldDecorator() 返回值是一个高阶组件
+                                // getFieldDecorator()() 返回一个新组件，新组件内部就会给传入的组件定义校验规则
+                                // getFieldDecorator(标识名称，配置对象)(组件)
+                                // 配置对象 --> 属性名固定的对象
+                                getFieldDecorator('username', {
+                                    // 表单校验的规则
+                                    rules: [
+                                        { required: true, whitespace: true, message: '必须输入用户名' },
+                                        { min: 4, message: '用户名必须大于4位' },
+                                        { max: 12, message: '用户名必须小于12位' },
+                                        { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数组或下划线组成' }
+                                    ]
+                                })(
+                                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
+                                )
+                            }
+                        </Item>
+                        <Item>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        // 自定义表单校验规则
+                                        { validator: this.validator }
+                                    ]
+                                })(
+                                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+                                )
+                            }
+                        </Item>
+                        <Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                            </Button>
+                        </Item>
+                    </Form>
                 </section>
             </div>
         )
     }
-}
+};
+export default Login
